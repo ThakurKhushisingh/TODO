9401a3a84a059751071c473547cb33c29f4428d3
Khushi Singh <122673305+ThakurKhushisingh@users.noreply.github.com>
2023-12-19 18:31:42 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInputtitle").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  var tx = document.createTextNode(
+""  );
+  li.appendChild(tx);
+
+  var inputValuedetail = document.getElementById("myInputDetail").value;
+  var tx = document.createTextNode(inputValuedetail);
+  li.appendChild(tx);
+  if (inputValue === "" || inputValuedetail === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInputtitle").value = "";
+  document.getElementById("myInputDetail").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+
+    };
+  }
+}
